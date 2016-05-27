c95bc563bb35e76c92bf9c48a34bdd0cff47c335
Arley Prates <arleyprates@gmail.com>
2016-05-27 02:48:00 -0300

    Add new methods in kdvoce-webservice

--- kdvoce-webservice.js
+++ kdvoce-webservice.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 var jsonfile = require('jsonfile');
-var file = 'kdvoce-location.json';
+var file_location = 'kdvoce-location.json';
+var file_solicitation = 'kdvoce-solicitation.json';
 var bodyParser = require('body-parser')
 
 /*
@@ -15,6 +16,39 @@ var bodyParser = require('body-parser')
 
 //Object para parser
 app.use(bodyParser.json());
+
+app.post('/solicitaPermissao', function (req, res) {
+  console.log('Lendo JSON');
+  fs.readFile( __dirname + "/" + "kdvoce-solicitation.json", 'utf8', function (err, data) {
+    console.log( 'post => solicitaPermissao' );
+    console.log( 'Criando object ARRAY' );
+    var data = JSON.parse( data );  
+    
+    console.log( 'Adicionando no ARRAY' );
+    console.log( 'Utilizando parser para receber os dados' );
+    data.push(req.body);
+
+    console.log( 'Atributos do array' );
+    console.log( req.body.id_telefone );
+    
+    console.log( 'Gravando JSON' );
+    jsonfile.writeFileSync(file_solicitation, data, {spaces: 2})
+
+    console.log( 'Retornando JSON' );
+    res.end( JSON.stringify(data));
+  });
+})
+
+app.get('/listaPermissoes', function (req, res) {
+  console.log('Lendo JSON');
+  fs.readFile( __dirname + "/" + "kdvoce-solicitation.json", 'utf8', function (err, data) {
+    data = JSON.parse( data );
+    console.log( 'Retornando JSON' );
+    console.log( data )
+    res.setHeader('Content-Type', 'application/json');
+    res.end( JSON.stringify( data ));
+  });
+})
 
 app.post('/addLocation', function (req, res) {
 
@@ -33,7 +67,7 @@ app.post('/addLocation', function (req, res) {
     console.log( req.body.longitude );
     
     console.log( 'Gravando JSON' );
-    jsonfile.writeFileSync(file, data, {spaces: 2})
+    jsonfile.writeFileSync(file_location, data, {spaces: 2})
 
     console.log( 'Retornando JSON' );
     res.end( JSON.stringify(data));
